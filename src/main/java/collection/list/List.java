7f52485d5e7861000f68954a5290e1afa4d968a1
xiarui001 <913246053@qq.com>
2019-07-07 17:55:56 +0800

    aa

--- src/main/java/collection/list/List.java
+++ src/main/java/collection/list/List.java
@@ -6,27 +6,8 @@ import java.util.Collections;
 public class List {
 
     public static void main(String[] args) {
-        int x = 0x7fffffff+1;
-        ArrayList<String> arrayList = new ArrayList<String>();
-        arrayList.add("aaa");
-        arrayList.add("bbb");
-        arrayList.add("ccc");
-        arrayList.add(null);
-        Object[] array = arrayList.toArray();
-        String[] strings = {"d","x","a","e","f","q"};
-        String[] array1 = arrayList.toArray(strings);
-        int length = array1.length;
-        System.out.println(array1[3]==null&&array1[4]==null);
-        System.out.println("array:"+array);
-        System.out.println("array1:"+array1);
-        System.out.println(arrayList.toString());
-        for (String s:arrayList) {
-            System.out.println(s);
-        }
+        ArrayList list = new ArrayList(0);
 
-        arrayList.forEach(s-> System.out.println(s));
-        arrayList.stream().filter(s -> {
-            return s.equals("aa");
-        }).count();
     }
+
 }
